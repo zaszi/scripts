25f2c7ac3be9b2405592af385a2108c57b263f47
zaszi <admin@zaszi.net>
2020-09-24 22:06:50 +0200

    Add Pandoc markdown-to-pdf script

---
+++ bin/gen_pdf.sh
@@ -0,0 +1,45 @@
+#!/bin/env bash
+
+# gen_pdf.sh
+#
+# Author: Zaszi - admin@zaszi.net
+#
+# A generator script to convert a Github-Flavored Markdown file to PDF using
+# Pandoc.
+#
+# Source:
+# https://learnbyexample.github.io/customizing-pandoc/
+#
+# Requirements:
+# - pandoc 2.10.1 or above
+# - texlive-most
+# - texlive-latexextra
+#
+# Usage: ./gen_pdf.sh <input_file>.md <output_file>.pdf
+
+# DEBUG
+# set -o xtrace
+
+set -o nounset
+set -o pipefail
+set -o errexit
+
+main() {
+    pandoc "$1" \
+        --from gfm \
+        --include-in-header snippets/chapter_break.tex \
+        --include-in-header snippets/inline_code.tex \
+        --include-in-header snippets/bullet_style.tex \
+        --highlight-style snippets/pygments.theme \
+        --variable=linkcolor:blue \
+        --variable=geometry:a4paper \
+        --variable=geometry:margin=2cm \
+        --variable=mainfont="Droid Serif" \
+        --variable=monofont="Iosevka Term Nerd Font Complete" \
+        --variable=fontsize=10pt \
+        --standalone \
+        --pdf-engine=xelatex \
+        --output "$2"
+}
+
+main "$@"
